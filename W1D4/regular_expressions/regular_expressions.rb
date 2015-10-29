569db709a20094f9ecb1383c2f448ef8d9517440
Stacey Lin <twstacey@gmail.com>
2015-10-29 15:05:53 -0700

    Added benefits of driver code to comments

--- W1D4/regular_expressions/regular_expressions.rb
+++ W1D4/regular_expressions/regular_expressions.rb
@@ -22,13 +22,11 @@ def grab_sin(string)
   sin_number
 end
 
-
 puts "grab_sin returns an SIN if the string has an SIN"
 puts grab_sin("please don't share this: 234-604-142") == "234-604-142"
 
 puts "grab_sin returns nil if it doesn't have a SIN"
 puts grab_sin("please confirm your identity: XXX-XXX-142") == nil
-
 
 # Return all of the SINs from a string, not just one.
 def grab_all_sins(string)
@@ -45,6 +43,8 @@ puts grab_all_sins("234-604-142, 350-802-074, 013-630-876") == ["234-604-142", "
 puts "grab_all_sins returns an empty Array if it doesn't have any SINs"
 puts grab_all_sins("please confirm your identity: XXX-XXX-142") == []
 
+puts "grab_all_sins returns single SIN if string has one SIN"
+puts grab_all_sins("please confirm your identity: 123-456-789") == ["123-456-789"]
 
 # Obfuscate all of the Social Insurance numbers in a string. Example: XXX-XX-4430.
 def hide_all_sins(string)
@@ -70,9 +70,22 @@ end
 puts "format_sins finds and reformat any SINs in the string"
 puts format_sins("234600142, 350.800.074, 013-600-876") == "234-600-142, 350-800-074, 013-600-876"
 
+string = "794314131, 143.413414, 143143.555"
+puts format_sins(string) == "794-314-131, 143-413-414, 143-143-555"
+
 puts "format_sins does not alter a string without SINs in it"
 string = "please confirm your identity: 4421422"
 puts format_sins(string) == string
 
 string = "please confirm your identity: 123abc445"
 puts format_sins(string) == string
+
+# ADDING DRIVER CODE
+# Adding driver code helps to solve problems because you can easily see if 
+# your method is achieving the results you want.
+
+# When you make changes, driver code allows you to check that everything is
+# still working as expected. (Changes have not boken previously working code.)
+
+# Starting with the expected results before you write a method helps to 
+# clarify the purpose of the code. (Helps to simplify what is needed.)
