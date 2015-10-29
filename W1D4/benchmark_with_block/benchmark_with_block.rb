56daa7bb4bc555ae55d91210c041ba4113513d1c
Stacey Lin <twstacey@gmail.com>
2015-10-29 11:20:39 -0700

    Benchmark with Block exercise

---
+++ W1D4/benchmark_with_block/benchmark_with_block.rb
@@ -0,0 +1,13 @@
+def benchmark
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  running_time = end_time - start_time
+end
+ 
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple" * 100000000
+ 
+running_time = benchmark { long_string.reverse }
+puts "string.reverse took #{running_time} seconds to run"
