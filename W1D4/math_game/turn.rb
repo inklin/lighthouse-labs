dee9db0b95e425536ab0ac772c193d6140d2044f
inklin <twstacey@gmail.com>
2015-11-01 00:57:20 +0000

    Move .to_i method to Question class

--- W1D4/math_game/turn.rb
+++ W1D4/math_game/turn.rb
@@ -9,7 +9,7 @@ class Turn
     puts "#{@player.name}: #{@question.get_question}".colorize(@player.color).on_black
     answer = @player.get_answer
 
-    answer.include?("quit") ? exit(0) : answer = answer.to_i
+    exit(0) if answer.include?("quit")
     
     if @question.correct?(answer)
       @player.add_score
