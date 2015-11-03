d2dd1ba31b82e8f4bd72d71d42f96450351520ed
Stacey Lin <twstacey@gmail.com>
2015-11-02 16:39:47 -0800

    Add test for Mammal

--- W2D1/classical_inheritance/tests/mammal_spec.rb
+++ W2D1/classical_inheritance/tests/mammal_spec.rb
@@ -5,4 +5,8 @@ describe Mammal do
   it "should create Mammals" do
     expect(Mammal.new).to be_a Mammal
   end
+  
+  it "should be a vertebrate" do
+    expect(Mammal.new.is_vertebrate?).to be true
+  end
 end
