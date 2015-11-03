4e7822867d021f236bc758e034f3a8978f553ce0
Stacey Lin <twstacey@gmail.com>
2015-11-02 16:46:09 -0800

    Revert "Initial commit"
    This reverts commit 13ef8a48582461110a9d36062ee3b4a5f7e6d088.

--- W2D1/contact_list/contact_database.rb
+++
@@ -1,2 +0,0 @@
-## TODO: Implement CSV reading/writing
-require 'csv'
--- W2D1/contact_list/contact_list.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'contact'
-require_relative 'contact_database'
-
-# TODO: Implement command line interaction
-# This should be the only file where you use puts and gets
