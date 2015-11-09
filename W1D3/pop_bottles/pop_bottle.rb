f8fafbee15f79df3bb29c752a01105f790b7209a
Stacey Lin <twstacey@gmail.com>
2015-11-08 21:13:00 -0700

    Refactor code after code review

--- W1D3/pop_bottles/pop_bottle.rb
+++ W1D3/pop_bottles/pop_bottle.rb
@@ -1,24 +1,20 @@
 CAP_TO_REDEEM = 4
 BOTTLE_TO_REDEEM = 2
+BOTTLE_COST = 2
 
 @total_bottles = 0 
 @bottles = 0
 @caps = 0
 
-# updates number of pops
 def buy_bottles(investment)
-  bottle_cost = 2
-  bought_bottles = investment / bottle_cost
+  bought_bottles = investment / BOTTLE_COST
   @total_bottles = bought_bottles
   @bottles = bought_bottles
   @caps = bought_bottles
   @free_from_caps = 0
   @free_from_bottles = 0
 
-  while can_redeem?
-    redeem
-  end
-
+  redeem
   show_stats(investment, bought_bottles)
 end
 
@@ -32,13 +28,11 @@ def show_stats(investment, bought_bottles)
   puts "Left over bottles: #{@bottles}"
 end
 
-# Return total # of new bottles, after giving in bottles and caps
 def give_bottles_and_caps(bottles, caps)
-  from_bottles = (bottles / BOTTLE_TO_REDEEM)
-  from_caps = (caps / CAP_TO_REDEEM)
+  from_bottles = bottles / BOTTLE_TO_REDEEM
+  from_caps = caps / CAP_TO_REDEEM
   update_freebies(from_bottles, from_caps)
-  new_bottles = from_bottles + from_caps
-  return new_bottles
+  from_bottles + from_caps
 end
 
 def update_freebies(from_bottles, from_caps)
@@ -46,12 +40,10 @@ def update_freebies(from_bottles, from_caps)
   @free_from_caps += from_caps
 end
 
-# Get leftover bottles
 def change_for_bottles(bottles)
   bottles % BOTTLE_TO_REDEEM
 end
 
-# Get left over caps
 def change_for_caps(caps)
   caps % CAP_TO_REDEEM
 end
@@ -61,25 +53,20 @@ def redeem
   @bottles = new_bottles + change_for_bottles(@bottles)
   @caps = new_bottles + change_for_caps(@caps)
   @total_bottles += new_bottles
+  redeem if can_redeem?
 end
 
-def can_redeem?()
+def can_redeem?
   @bottles >= BOTTLE_TO_REDEEM || @caps >= CAP_TO_REDEEM
 end
 
-def run_bottle_info()
-  done = false
-
-  until done do
+def run_bottle_info
+  loop do
     print "How much do you want to spend on pop? >> "
     answer = gets.chomp
-
-    if answer.include?('quit')
-      done = true
-    else
-      dollar_amount = answer.to_i
-      buy_bottles(dollar_amount)
-    end
+    break if answer.include?('quit')
+    dollar_amount = answer.to_i
+    buy_bottles(dollar_amount)
   end
 end
 
