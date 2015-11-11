246fc8b706452e906f1d3215ac32b80ef0bfa684
inklin <twstacey@gmail.com>
2015-11-11 20:58:20 +0000

    Add exercise 5

--- W3D3/ar-exercises/exercises/exercise_5.rb
+++ W3D3/ar-exercises/exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+avg_revenue = Store.average(:annual_revenue)
+puts "Total revenue for the Company: $#{total_revenue}"
+puts "Average revenue for all stores: $#{avg_revenue.floor}"
+
+million_dollar_stores = Store.where("annual_revenue > ?", [1_000_000])
+puts "The number of stores with $1 million+ in annual revenue: #{million_dollar_stores.count}"
+
+million_dollar_stores.each do |store|
+  puts "Store Name: #{store.name} Annual Revenue #{store.annual_revenue}"
+end
