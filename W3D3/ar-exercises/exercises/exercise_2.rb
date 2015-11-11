f9e55d5b6d2f7b47b163158c0a4616c0c78e1358
inklin <twstacey@gmail.com>
2015-11-10 23:40:00 -0700

    Complete Exercise 1 and 2

---
+++ W3D3/ar-exercises/exercises/exercise_2.rb
@@ -0,0 +1,10 @@
+require_relative '../setup'
+require_relative './exercise_1'
+
+puts "Exercise 2"
+puts "----------"
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.update(name: 'Burnaby Store')
