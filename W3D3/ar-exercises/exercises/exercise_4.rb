1ac5d4e95dd064737a5860f7d1b6f4960db48943
inklin <twstacey@gmail.com>
2015-11-11 20:33:47 +0000

    Add exercise 3 and 4

--- W3D3/ar-exercises/exercises/exercise_4.rb
+++ W3D3/ar-exercises/exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# Expect Burnaby, Whistler and Yaletown
+@mens_stores.each do |store|
+  puts "Store name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true, )
+                      .where("annual_revenue > ?", [1_000_000])
+
+# Expect only Richmond Store
+@womens_stores.each do |store|
+  puts "Store name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end
