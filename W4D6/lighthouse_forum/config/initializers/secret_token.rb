3f08989f728db7cc09d1fa95c7d38c46340e217d
Stacey Lin <twstacey@gmail.com>
2015-11-21 23:01:04 -0800

    Routing /posts to /posts/index.html.erb

---
+++ W4D6/lighthouse_forum/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '8f7c902cdcab5a1a57d9a929409758db3c56e54d66627033e749aba05cbc4d3c9358f8a6f8a72d326af1fe00c34d35c377ac343539a76dd41009292c7d5e7c24'
