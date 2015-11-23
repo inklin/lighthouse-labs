ba1f6154a2458cfffc65966f18577a7062eebcb3
inklin <twstacey@gmail.com>
2015-11-23 20:27:15 +0000

    Add edit post functionality

--- W4D6/lighthouse_forum/app/controllers/posts_controller.rb
+++ W4D6/lighthouse_forum/app/controllers/posts_controller.rb
@@ -26,6 +26,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
   protected
 
   def post_params
--- W4D6/lighthouse_forum/config/routes.rb
+++ W4D6/lighthouse_forum/config/routes.rb
@@ -1,10 +1,12 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts',      to: 'posts#index'
-  get 'posts/new',  to: 'posts#new'
-  get 'posts/:id',  to: 'posts#show'
-  post 'posts',     to: 'posts#create'
-  
+  get 'posts',          to: 'posts#index'
+  get 'posts/new',      to: 'posts#new'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id',      to: 'posts#show', as: 'post'
+  patch 'posts/:id',    to: 'posts#update'
+  post 'posts',         to: 'posts#create'
+
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
