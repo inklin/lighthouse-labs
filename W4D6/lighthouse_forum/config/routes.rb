c05588c31b1acc1019c382486d5cce1fdf0b4f81
inklin <twstacey@gmail.com>
2015-11-23 21:58:24 +0000

    Refactor routes to use resources

--- W4D6/lighthouse_forum/config/routes.rb
+++ W4D6/lighthouse_forum/config/routes.rb
@@ -1,11 +1,6 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts',          to: 'posts#index'
-  get 'posts/new',      to: 'posts#new'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id',      to: 'posts#show', as: 'post'
-  patch 'posts/:id',    to: 'posts#update'
-  post 'posts',         to: 'posts#create'
+  resources :posts
 
 end
 
