1439a195ebf708206a4e97e6f26d4e5f3edf4d23
inklin <twstacey@gmail.com>
2015-11-23 20:14:01 +0000

    Add new post functionality

--- W4D6/lighthouse_forum/app/controllers/posts_controller.rb
+++ W4D6/lighthouse_forum/app/controllers/posts_controller.rb
@@ -8,5 +8,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title:  params[:post][:title],
+      author: params[:post][:author],
+      text:   params[:post][:text]
+    )
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
 
 end
--- W4D6/lighthouse_forum/config/routes.rb
+++ W4D6/lighthouse_forum/config/routes.rb
@@ -1,8 +1,10 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts', to: 'posts#index'
-  get 'posts/:id', to: 'posts#show'
-
+  get 'posts',      to: 'posts#index'
+  get 'posts/new',  to: 'posts#new'
+  get 'posts/:id',  to: 'posts#show'
+  post 'posts',     to: 'posts#create'
+  
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
