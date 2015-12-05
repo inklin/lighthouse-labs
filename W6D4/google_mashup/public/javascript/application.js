5342711cd5de30da495cd90dc90b12d69f0bb9ba
inklin <twstacey@gmail.com>
2015-12-05 01:08:54 +0000

    Add list items for wonders

--- W6D4/google_mashup/public/javascript/application.js
+++ W6D4/google_mashup/public/javascript/application.js
@@ -1,113 +1,122 @@
-function initMap() {
-
-  var wonders = [
+var wonders = [
   {
-    "name": "Chichen Itza",
-    "lat": 20.684294,
-    "lng": -88.567784,
-    "content": "Hello!"
+    name: "Chichen Itza",
+    country: "Mexico",
+    lat: 20.684294,
+    lng: -88.567784,
+    description: "A large pre-Columbian city built by the Maya people of the Terminal Classic period. (c. AD 800-900)",
   },
   {
-    "name": "Christ the Redeemer",
-    "lat": -22.951590,
-    "lng": -43.210455,
-    "content": "Bye!"
+    name: "Christ the Redeemer",
+    country: "Brazil",
+    lat: -22.951590,
+    lng: -43.210455,
+    description: "A statue of Jesus Christ in Rio de Janeiro, Brazil. It is 30m tall, with an 8m pedestal and overlooks the city of Rio."
   },
   {
-    "name": "Great Wall of China",
-    "lat": 40.432128,
-    "lng": 116.570343
+    name: "Great Wall of China",
+    country: "China",
+    lat: 40.432128,
+    lng: 116.570343,
+    description: "The wall lines the historical northern borders of China, and was built to defend China against invasions. The entire wall measures to be 21, 196km in total length."
   },
   {
-    "name": "Machu Pichu",
-    "lat": -13.162943, 
-    "lng": -72.544941
+    name: "Machu Pichu",
+    country: "Peru",
+    lat: -13.162943, 
+    lng: -72.544941,
+    description: "A 15th-century Inca site located 2,430m above sea level. It is the most familiar icon of Inca civilization."
   },
   {
-    "name": "Petra",
-    "lat": 30.329298,
-    "lng": 35.444676
+    name: "Petra",
+    country: "Jordan",
+    lat: 30.329298,
+    lng: 35.444676,
+    description: "Also known as the Rose City, Petra is a historical and archaeological city that is famous for its rock-cut architecture and water conduit system."
   },
   {
-    "name": "Taj Mahal",
-    "lat": 27.175340,
-    "lng": 78.042112
+    name: "Taj Mahal",
+    country: "India",
+    lat: 27.173168,
+    lng: 78.042068,
+    description: "The Taj Mahal is a white marble mausoleum commissioned in 1632 by the Mughal emperor Shah Jahan. It is widely regarded as 'the jewel of Muslim art in India'."
   },
   {
-    "name": "Colosseum",
-    "lat": 41.890450,
-    "lng": 12.492252
+    name: "Colosseum",
+    country: "Italy",
+    lat: 41.890450,
+    lng: 12.492252,
+    description: "The Colosseum, also known as the Flavian Amphitheatre, is an amphitheatre built of concrete and sand. It is the largest amphitheatre ever built and considered one of the greatest works of architecture and engineering."
   }
-  ]
+  ];
 
-  var markers = [];
+var map;
+var pano;
+var markers = [];
+var firstWonder = { lat: 27.173168, lng: 78.042068 };
 
-  var theirLatLng = { lat: -25.363, lng: 131.044 };
-  var myLatLng = { lat: 49.287966, lng: -123.143435};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 3,
-    center: myLatLng
+function initMap() {
+  // Set up the map
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: firstWonder,
   });
 
-    // Go through each wonder
-  $.each(wonders, function(index, wonder){
+  // Set up Streetview
+  var sv = new google.maps.StreetViewService();
+
+  sv.getPanorama({location: firstWonder, radius: 50}, processSVData);
+
+  // Set up Pano
+  var panoOptions = {
+    navigationControl: true,
+    position: firstWonder
+  }
+
+  pano = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoOptions);
+
+  // Go through each wonder and add markers/info windows
+  wonders.forEach(function(wonder){
     var marker = new google.maps.Marker({
       position: { lat: wonder.lat, lng: wonder.lng },
       map: map
     });
     markers.push(marker);
 
+    var infoContent = "<div class='content'>" +
+      '<h4>' + wonder.name + ' | ' + wonder.country + '</h4>' +
+      '</div>';
+
     var infoWindow = new google.maps.InfoWindow({
-       content: wonder.content
+       content: infoContent
     });
 
     marker.addListener("click", function(){
+      map.panTo(marker.position);
       infoWindow.open(map, marker);
-    })
+      sv.getPanorama({location: marker.position, radius: 50}, processSVData)
+    });
   })
 
-  // var theirMarker = new google.maps.Marker({
-  //   position: theirLatLng,
-  //   map: map,
-  //   title: 'Hello World!'
-  // });
-
-  // var myMarker = new google.maps.Marker({
-  //   position: myLatLng,
-  //   map: map,
-  //   title: 'English Bay Beach!',
-  //   draggable: true,
-  //   animation: google.maps.Animation.DROP,
-  // })
-  // var contentString = '<div id="content">'+
-  //   '<div id="siteNotice">'+
-  //   '</div>'+
-  //   '<h1 id="firstHeading" class="firstHeading">English Bay Beach</h1>'+
-  //   '<div id="bodyContent" class="body-content">'+
-  //   '<p><b>English Bay Beach</b>, also called <b>First Beach</b>, is the most' +
-  //   'populated beach area in Vancouver\' downtown area. The Stanley Park Seawall,' +
-  //   ' a popular running and biking route, runs along the east side of the beach.</p>' +
-  //   '<p>Attribution: English Bay Beach, <a href="https://en.wikipedia.org/wiki/English_Bay_(Vancouver)">'+
-  //   'English Bay (Vancouver) - Wikipedia</a> '+
-  //   '(last visited Dec 3, 2015).</p>'+
-  //   '</div>'+
-  //   '</div>';
-
-  // var infoWindow = new google.maps.InfoWindow({
-  //   content: contentString
-  // })
-
-  // myMarker.addListener("click", function(){
-  //   toggleBounce();
-  //   infoWindow.open(map, myMarker);
-  // });
-
-  // function toggleBounce() {
-  //   if (myMarker.getAnimation() !== null) {
-  //     myMarker.setAnimation(null);
-  //   } else {
-  //     myMarker.setAnimation(google.maps.Animation.BOUNCE);
-  //   }
-  // }
+  addWonderList();
 }
+
+function processSVData(data, status){
+  if (status === google.maps.StreetViewStatus.OK){
+    pano.setPano(data.location.pano);
+    pano.setPov({
+      heading: 270,
+      pitch: 0
+    });
+    pano.setVisible(true);
+  }
+}
+
+function addWonderList(){
+  var $list = $("<ul></ul>");
+  wonders.forEach(function(wonder){
+    $list.append("<li>" + wonder.description + "</li>");
+  });
+  $("#menu").append($list);
+}
+
