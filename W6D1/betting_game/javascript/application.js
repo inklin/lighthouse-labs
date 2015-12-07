cf0891a67d57a592d15dfeca2295f541c2be00a1
Stacey Lin <twstacey@gmail.com>
2015-12-06 22:04:55 -0800

    Added placemat for card, added button to add money

--- W6D1/betting_game/javascript/application.js
+++ W6D1/betting_game/javascript/application.js
@@ -28,13 +28,13 @@ $(document).ready(function(){
   function checkGuess(guess, betAmount) {
     var difference = Math.abs(guess - answer);
     if (guess === answer) {
-      $("#instruction").text("You are correct!");
+      $("#instruction").text("Winner! You just won " + betAmount * 7 + " dollars!");
       addToBankroll(betAmount);
       displayBalance();
     } else if (difference === 1){
       $("#instruction").text("You were off by 1. You get to keep your money.");
     } else {
-      $("#instruction").text("Boo, you were wrong");
+      $("#instruction").text("Better luck next round.");
       subtractFromBankroll(betAmount);
       displayBalance();
     }
@@ -53,12 +53,7 @@ $(document).ready(function(){
   }
 
   function displayBalance() {
-    $('#user-bankroll').text(userBankroll);
-  }
-
-  function getInput() {
-    var bet = parseInt($("#bet").val());
-    return { bet: bet, guess: guess };
+    $("#bankroll").text(userBankroll);
   }
 
   function showAnswer() {
@@ -75,20 +70,22 @@ $(document).ready(function(){
   }
 
   function guessValid(guess) {
-    return guess && (guess <= guessMax) && (guess >= guessMin);
+    return (guess !== null && guess <= guessMax && guess >= guessMin);
   }
 
   function setupBet() {
-    $(".card.is-chosen").removeClass("is-chosen");
-    $(".flip-container").removeClass("flipped");
     $("#instruction").text("Pick a card!");
     guess = null;
     var newAnswer = getRandomNumber(guessMin, guessMax);
     createAnswerCard(newAnswer);
   }
 
-  function validateInputs() {
-    var input = getInput();
+  function getInput() {
+    var bet = parseInt($("#bet").val());
+    return { bet: bet, guess: guess };
+  }
+
+  function validateInputs(input) {
     var errors = [];
 
     if (betValid(input.bet) && sufficientFunds(input.bet) && guessValid(input.guess)) {
@@ -97,43 +94,66 @@ $(document).ready(function(){
 
     if (!betValid(input.bet)) {
       errors.push("Bets must be greater than 0.");
-    }
-    if (!sufficientFunds(input.bet)) {
+    } else if (!sufficientFunds(input.bet)) {
       errors.push("You have insufficient funds to place that bet.");
     }
+
     if (!guessValid(input.guess)) {
       errors.push("You have not guessed a number.");
     }
 
-    return errors;
+    if (errors.length > 0) {
+      $("#instruction").text(errors.join(" "));
+    }
+
+    return false;
   }
 
   function placeBet(input) {
     checkGuess(input.guess, input.bet);
     showAnswer();
-    setTimeout(function(){
-      setupBet();
+    setTimeout(function() {
+      hideCard();
     }, 1500);
+    setTimeout(function(){
+      if (userBankroll > 0) {
+        setupBet();
+      } else {
+        $("#instruction").text("You're out of money!");
+        $("#add-money").show();
+      }
+    }, 2000);
   }
 
-  setupBet();
+  function hideCard() {
+    $(".card.is-chosen").removeClass("is-chosen");
+    $(".flip-container").removeClass("flipped");
+  }
 
   $("#place-bet").on("click", function(){
-    placeBet();
+    var input = getInput();
+    var inputValid = validateInputs(input);
+
+    if (inputValid){
+      placeBet(input);
+    }
   });
 
   $(".guess-card").on("click", function() {
+    $(".is-chosen").removeClass("is-chosen");
     $(this).addClass("is-chosen");
     var guessString = $(this).attr("id").replace("card", '');
     guess = parseInt(guessString);
-    $("#instruction").text("Place a bet amount.")
+    $("#instruction").text("Place your bet.")
   });
 
+  $("#add-money").on("click", function() {
+    userBankroll = 100;
+    $("#bankroll").text(100);
+    $(this).hide();
+    $("#instruction").text("Thanks for adding $100. Pick a card!");
+  });
+
+  setupBet();
+
 });
-
-
-
-// do {
-//  runTurn();
-//  userBankrupt = bankrollEmpty();
-// } while (!userBankrupt);
