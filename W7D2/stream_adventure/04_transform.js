69f3d2442b884ea1581218954f5bd7566082842b
inklin <twstacey@gmail.com>
2015-12-09 05:35:18 +0000

    Exercise 2 and 4

---
+++ W7D2/stream_adventure/04_transform.js
@@ -0,0 +1,22 @@
+var through = require('through2');
+var transform = through(write, end);
+
+function write (buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+}
+
+function end (done) {
+  done();
+}
+
+process.stdin.pipe(transform).pipe(process.stdout);
+
+// Here is the reference solution:
+
+var through = require('through2');
+var tr = through(function (buf, _, next) {
+    this.push(buf.toString().toUpperCase());
+    next();
+});
+process.stdin.pipe(tr).pipe(process.stdout);
